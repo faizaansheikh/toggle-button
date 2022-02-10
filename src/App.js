ba231c1646ba25723197c727d37f964710f66462
faizaansheikh <princefaizan800@gmail.com>
2022-02-10 08:34:18 -0800

    Completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import ONIMAGE from "../src/onn.png"
+import OfIMAGE from "../src/off.png"
 
 function App() {
+
+   const [color,setcolor] = useState(false)
+   const [bulb,setbulb] = useState(false)
+
+   const colorSwitch = () => color? setcolor(false) : setcolor(true);
+   const bulbSwitch = () => bulb? setbulb(false) : setbulb(true);
+   
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div className={color? "classchange" : "classnotchange"}>
+   <h1 className={color? "nothead" : "head"}>Toggle switch</h1>
+   <button onClick={colorSwitch}>switch</button>
+   <button onClick={bulbSwitch}>bulb</button>
+      <img src={bulb? ONIMAGE : OfIMAGE} alt="" width="200px"/>
+      
+   </div>
+   </>
   );
 }
 
